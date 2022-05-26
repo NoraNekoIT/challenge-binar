fb053837d13bd0ba3d73c584c44f407d3a692051
NoraNekoIT <bunsmart18@gmail.com>
2022-05-26 14:11:11 +0700

    push deploy

--- src/main/java/com/noranekoit/challenge4/Challenge4Application.java
+++ src/main/java/com/noranekoit/challenge4/Challenge4Application.java
@@ -1,12 +1,9 @@
 package com.noranekoit.challenge4;
 
 
-import io.jsonwebtoken.Jwts;
+
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
-import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
-import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import io.swagger.v3.oas.annotations.info.Info;
-import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
